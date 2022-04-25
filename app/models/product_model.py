c4b166a29f2b1f9bb3a1f2e26e8ecac35eb66bd0
allan <allangreen2906@gmail.com>
2022-04-25 17:48:43 -0300

    feat: created products table

--- app/models/product_model.py
+++ app/models/product_model.py
@@ -1,1 +1,32 @@
-class ProductModel():...
+from dataclasses import dataclass
+
+from app.configs.database import db
+from sqlalchemy import VARCHAR, Boolean, Column, Integer, Numeric
+from sqlalchemy.orm import relationship, backref
+
+
+@dataclass
+class ProductModel(db.Model):
+    __tablename__ = 'products'
+
+    id: int
+    title: str
+    price: float
+    parent_id: int
+    description: str
+    image: str
+    sold: bool
+
+    id = Column(Integer, primary_key=True)
+    title = Column(VARCHAR(128), nullable=False)
+    price = Column(Numeric, nullable=False)
+    parent_id = Column(Integer, nullable=False)
+    description = Column(VARCHAR)
+    image = Column(VARCHAR)
+    sold = Column(Boolean, default=False)
+
+    categories = relationship(
+        'Category',
+        secondary='categories_products',
+        backref=backref('products')
+    )
