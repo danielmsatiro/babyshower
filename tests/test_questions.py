82d14fafd89379d8823b15594e5b6d8f97ede52f
Pedro Basilio <pedronobasi@gmail.com>
2022-04-25 16:36:36 -0300

    fix: nomenclature

--- tests/test_questions.py
+++ tests/test_questions.py
@@ -1,11 +1,11 @@
 from flask.testing import FlaskClient
 
 #POST
-def test_create_ask(client: FlaskClient):
+def test_create_question(client: FlaskClient):
     payload = {
         "question": "teste"
     }
-    response = client.post("/api/asks/1", json=payload)
+    response = client.post("/api/questions/1", json=payload)
     expected = {
         "id": 1,
         "question": "teste",
@@ -15,31 +15,31 @@ def test_create_ask(client: FlaskClient):
 
     assert (
         response.status_code == 201
-    ), "Check your status code at route 'post asks/<product_id>'"
+    ), "Check your status code at route 'post questions/<product_id>'"
     assert (
         response.json == expected
-    ), "Check your body request at route 'post asks/<product_id>'"
+    ), "Check your body request at route 'post questions/<product_id>'"
 
 #GET
-def test_get_asks(client: FlaskClient):
-    response = client.get("/api/asks/1")
+def test_get_question(client: FlaskClient):
+    response = client.get("/api/question/1")
     expected = [
             {"id": 1, "question": "teste", "product_id": 1, "parent_id": 1}
         ]
 
     assert (
         response.status_code == 200
-    ), "Check your status code at route 'get asks/<product_id>'"
+    ), "Check your status code at route 'get questions/<product_id>'"
     assert (
         response.json == expected
-    ), "Something went wrong at route 'get asks/<product_id>'"
+    ), "Something went wrong at route 'get questions/<product_id>'"
 
 #PATCH
-def test_patch_ask(client: FlaskClient):
+def test_patch_question(client: FlaskClient):
     payload = {
         "question": "teste atualizado"
     }
-    response = client.patch("api/asks/1", json=payload)
+    response = client.patch("api/questions/1", json=payload)
     expected = {
         "id": 1,
         "question": "teste atualizado",
@@ -49,22 +49,22 @@ def test_patch_ask(client: FlaskClient):
     
     assert (
         response.status_code == 201
-    ), "Check your status code at route 'patch asks/<ask_id>'"
+    ), "Check your status code at route 'patch questions/<question_id>'"
     assert (
         response.json == expected
-    ), "Something went wrong at route 'patch asks/<product_id>'"
+    ), "Something went wrong at route 'patch questions/<product_id>'"
 
 #DELETE 
-def test_delete_ask(client: FlaskClient):
-    response = client.delete("api/asks/1")
+def test_delete_question(client: FlaskClient):
+    response = client.delete("api/questions/1")
     expected = {"msg": "Question deleted"}
 
     assert (
         response.status_code == 200
-    ), "Check your status code at route 'patch asks/<ask_id>'"
+    ), "Check your status code at route 'patch questions/<question_id>'"
     assert (
         response.json == expected
-    ), "Something went wrong at route 'patch asks/<product_id>'"
+    ), "Something went wrong at route 'patch questions/<product_id>'"
 
 
 
