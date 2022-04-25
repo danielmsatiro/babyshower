73bb00fdeee85e3f8b3cf435d7ef26c6319de421
Pedro Basilio <pedronobasi@gmail.com>
2022-04-25 15:35:27 -0300

    feat: test questions

---
+++ tests/test_asks.py
@@ -0,0 +1,70 @@
+from flask.testing import FlaskClient
+
+#POST
+def create_ask(client: FlaskClient):
+    payload = {
+        "question": "teste"
+    }
+    response = client.post("/api/asks/1", json=payload)
+    expected = {
+        "id": 1,
+        "question": "teste",
+        "product_id": 1,
+        "parent_id": 1
+    }
+
+    assert (
+        response.status_code == 201
+    ), "Check your status code at route 'post asks/<product_id>'"
+    assert (
+        response.json == expected
+    ), "Check your body request at route 'post asks/<product_id>'"
+
+#GET
+def get_asks(client: FlaskClient):
+    response = client.get("/api/asks/1")
+    expected = [
+            {"id": 1, "question": "teste", "product_id": 1, "parent_id": 1}
+        ]
+
+    assert (
+        response.status_code == 200
+    ), "Check your status code at route 'get asks/<product_id>'"
+    assert (
+        response.json == expected
+    )
+
+#PATCH
+def patch_ask(client: FlaskClient):
+    payload = {
+        "question": "teste atualizado"
+    }
+    response = client.patch("api/asks/1", json=payload)
+    expected = {
+        "id": 1,
+        "question": "teste atualizado",
+        "product_id": 1,
+        "parent_id": 1
+    }
+    
+    assert (
+        response.status_code == 201
+    ), "Check your status code at route 'patch asks/<ask_id>'"
+    assert (
+        response.json == expected
+    )
+
+#DELETE 
+def delete_ask(client: FlaskClient):
+    response = client.delete("api/asks/1")
+    expected = {"msg": "Question deleted"}
+
+    assert (
+        response.status_code == 200
+    ), "Check your status code at route 'patch asks/<ask_id>'"
+    assert (
+        response.json == expected
+    )
+
+
+
