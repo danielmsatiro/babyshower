429810da53c6a04290cc5d1deab3da440212b653
Guilherme Couto <gcoutos95@gmail.com>
2022-04-26 13:38:22 -0300

    feature(answers/migration): Adding the latest migration version

--- app/models/answer_model.py
+++ app/models/answer_model.py
@@ -2,8 +2,6 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import backref, relationship
 from dataclasses import dataclass
 
-from app.configs.database import db
-
 from app.models.parent_model import ParentModel
 from app.models.ask_model import AskModel
 
