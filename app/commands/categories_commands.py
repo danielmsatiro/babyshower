9f4880936442ce48bbd72bbb8217f2f12c07bbb9
hirtonsantos <hirtonsantos.hs@gmail.com>
2022-04-26 15:17:15 -0300

    feat(categories): populate with cli and add column name

--- app/commands/categories_commands.py
+++ app/commands/categories_commands.py
@@ -16,17 +16,17 @@ def categories_cli():
     categories_description_list = [
         "Tudo o que o seu bebê precisa até os seus 3 meses",
         "Tudo o que o seu bebê precisa até os seus 6 meses",
-        "Tudo o que o seu bebê precisa até os seus 6 meses",
+        "Tudo o que o seu bebê precisa até os seus 9 meses",
         "Tudo o que o seu bebê precisa até os seus 1 ano de idade",
         "Tudo o que o seu bebê precisa até os seus 2 anos de idade",
         "Tudo o que o seu bebê precisa de 3 a 5 anos",
-        "Carrinhos para o seu bebê sem idade específica",
-        "Roupas para o seu bebê sem idade específica",
+        "Carrinhos para o seu bebê",
+        "Roupas para o seu bebê",
         "Acessórios para o quarto do seu bebê",
-        "Cadeiras de sol, de carro etc para ser usada pelo bebê",
+        "Cadeiras de sol, de carro etc. para ser usada pelo bebê",
         "Acessórios para a maternidade",
         "Acessórios para o banho de bebê do seu bebê",
-        "Brinquedos para o seu bebê sem idade específica",
+        "Brinquedos para o seu bebê",
         "Acessórios para alimentação e amamentação",
         "Acessórios para a segurança do bebê",
         "Acessórios para a saúde do bebê",
@@ -39,7 +39,10 @@ def categories_cli():
 
         for x in range(len(categories_names_list)):
             categories.append(
-                CategoryModel(description=categories_description_list[x]))
+                CategoryModel(
+                    name=categories_names_list[x],
+                    description=categories_description_list[x]
+                    ))
 
         session.add_all(categories)
         session.commit()
