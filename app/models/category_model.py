558754355a3bd47ab22de2a30ee66ee4024237c7
hirtonsantos <hirtonsantos.hs@gmail.com>
2022-04-25 23:18:52 -0300

    feat: add models without migrations

--- app/models/category_model.py
+++ app/models/category_model.py
@@ -1,1 +1,12 @@
-class CategoryModel():...
+from sqlalchemy import Column, ForeignKey, Integer, Text
+from dataclasses import dataclass
+from app.configs.database import db
+
+
+@dataclass
+class Categories(db.Model):
+
+    __tablename__ = "Categories"
+
+    id = Column(Integer, primary_key=True)
+    description = Column(Text, default="")
--- app/models/category_product_table.py
+++ app/models/category_product_table.py
@@ -1,1 +1,13 @@
-category_product = ...
+from app.configs.database import db
+from dataclasses import dataclass
+from sqlalchemy import Column, Integer, ForeignKey
+
+
+@dataclass
+class CategoryProductModel(db.Model):
+
+    __tablename__ = "category_product"
+
+    product_id = Column(Integer, ForeignKey("product.id"), nullable=False)
+    categories_id = Column(
+        Integer, ForeignKey("categories.id"), nullable=False)
