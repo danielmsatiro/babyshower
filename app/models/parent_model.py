4b1b4037be76f6f1f7ac597fa7c1100d8cf7f840
felipefoster <88387736+felipefoster@users.noreply.github.com>
2022-04-26 16:43:26 -0300

    feat(parents):
     add logic password and change column password to password_hash

--- app/models/parent_model.py
+++ app/models/parent_model.py
@@ -1,32 +1,41 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String,ForeignKey
 from dataclasses import dataclass
-from sqlalchemy.sql.schema import UniqueConstraint
 from app.configs.database import db
-
 from sqlalchemy.orm import relationship, backref
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 @dataclass
 class ParentModel(db.Model):
 
     __tablename__ = "parents"
 
-    cpf: int
+    username: str
 
     cpf = Column(Integer, primary_key=True, nullable=False)
-    username: str = Column(String, nullable=False)
+    username = Column(String, nullable=False)
     name = Column(String, nullable=False)
     email = Column(String, nullable=False)
-    password = Column(String, nullable=False)
+    password_hash = Column(String, nullable=False)
     phone = Column(String, nullable=False)
     
+    products = relationship(
+        'ProductModel',
+        backref=backref('parent', uselist=False)
+    )
 
-    # c = relationship(
-        #     '',
-        #     secondary='',
-        #     backref=backref('')
-        # )
-    
     questions = relationship(
         'QuestionModel', 
-        backref=backref('parent')
+        backref=backref('parent', uselist=False)
     )
+
+    @property
+    def password(self):
+        raise AttributeError("Password cannot be accessed")
+    
+    @password.setter
+    def password(self, password_to_hash):
+        self.password_hash = generate_password_hash(password_to_hash)
+
+    def verify_password(self, password_to_compare):
+        return check_password_hash(self.password_hash, password_to_compare)
