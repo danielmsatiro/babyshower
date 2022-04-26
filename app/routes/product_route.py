3ccf4cb7e5b8a2bc87f1649203f82b89553de53d
hirtonsantos <hirtonsantos.hs@gmail.com>
2022-04-26 02:00:31 -0300

    feat: add routes to update, create and delete product

--- app/routes/product_route.py
+++ app/routes/product_route.py
@@ -1,9 +1,12 @@
-from app.controllers.product_controller import get_all, get_by_id, get_by_parent, get_by_params
+from app.controllers import product_controller
 from flask import Blueprint
 
 bp = Blueprint('bp_products', __name__, url_prefix='/products')
 
-bp.get('')(get_all)
-bp.get('/params')(get_by_params)
-bp.get('/<int:product_id>')(get_by_id)
-bp.get('/by_parent/<int:parent_id>')(get_by_parent)
+bp.post('')(product_controller.create_product)
+bp.get('')(product_controller.get_all)
+bp.get('/params')(product_controller.get_by_params)
+bp.get('/<int:product_id>')(product_controller.get_by_id)
+bp.get('/by_parent/<int:parent_id>')(product_controller.get_by_parent)
+bp.patch('/<int:product_id>')(product_controller.update_product)
+bp.delete('/<int:product_id>')(product_controller.delete_product)
