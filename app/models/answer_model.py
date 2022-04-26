06ca098f9c3be5bd19bf93ebbff2c400466e4c12
Guilherme Couto <gcoutos95@gmail.com>
2022-04-26 09:36:43 -0300

    feat(answer/model/migration): Adding the migrations and answer model

--- app/models/answer_model.py
+++ app/models/answer_model.py
@@ -1,2 +1,48 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import backref, relationship
+from dataclasses import dataclass
+
+from app.configs.database import db
+
+from app.models.parent_model import ParentModel
+from app.models.ask_model import AskModel
+
+@dataclass
 class AnswerModel():
-    ...
+    id = int
+    answer = str
+    parent = ParentModel
+    question = AskModel
+
+    __tablename__ = "answers"
+
+    id = Column(Integer, primary_key=True)
+    answer = Column(String(150))
+
+    parent_id = Column(
+        ForeignKey('parents.id'),
+        nullable=False,
+        unique=True
+    )
+
+    question_id = Column(
+        ForeignKey('questions.id'),
+        nullable=False,
+        unique=True
+    )
+
+    parent = relationship(
+        "ParentModel",
+        backref=backref(
+            "answer",
+            use_list=False
+        )
+    )
+
+    question = relationship(
+        "AskModel",
+        backref=backref(
+            "answer",
+            use_list=False
+        )
+    )
