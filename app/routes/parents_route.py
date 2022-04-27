a1501adf428fc3c80cc01d01a9bde4f88fcf3718
Daniel M Satiro <mateus_satiro@yahoo.com.br>
2022-04-27 11:46:03 -0300

    fix[login]: change function called

--- app/routes/parents_route.py
+++ app/routes/parents_route.py
@@ -3,11 +3,13 @@ from flask import Blueprint
 
 bp = Blueprint("bp_parents", __name__, url_prefix="/parents")
 
+
 def test_get_parents():
     ...
 
-bp.get('')(parents_controller.pick_parents)
-bp.post('')(parents_controller.new_parents)
-bp.post('/login')(parents_controller.new_parents)
-bp.patch('/<parent_cpf>')(parents_controller.update_parents)
-bp.delete('/<parent_cpf>')(parents_controller.delete_parents)
+
+bp.get("")(parents_controller.pick_parents)
+bp.post("")(parents_controller.new_parents)
+bp.post("/login")(parents_controller.login)
+bp.patch("/<parent_cpf>")(parents_controller.update_parents)
+bp.delete("/<parent_cpf>")(parents_controller.delete_parents)
