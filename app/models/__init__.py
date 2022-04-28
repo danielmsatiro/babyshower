a8b366ba535635ace8f0a9592addb438077b94c4
Daniel M Satiro <mateus_satiro@yahoo.com.br>
2022-04-28 14:22:31 -0300

    fix[models]: fix import error

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,4 @@
 from app.models.answer_model import AnswerModel
-from app.models.ask_model import AskModel
 from app.models.category_model import CategoryModel
 from app.models.category_product_table import category_product
 from app.models.parent_model import ParentModel
