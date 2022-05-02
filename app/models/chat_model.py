e08501d008f86ef7c96ac014cb131eff3b58ff67
Daniel M Satiro <mateus_satiro@yahoo.com.br>
2022-05-02 15:52:16 -0300

    feat[chat_model]: add ChatModel

--- app/models/chat_model.py
+++ app/models/chat_model.py
@@ -1,5 +1,27 @@
+from dataclasses import dataclass
+from datetime import datetime
+
 from app.configs.database import db
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String
 
 
+@dataclass
 class ChatModel(db.Model):
-    ...
+    id: int
+    data: datetime
+    message: str
+    confirmed_read: bool
+    reader_id: int
+    writer_id: int
+
+    __tablename__ = "chat"
+    id = Column(Integer, primary_key=True)
+    data = Column(DateTime, nullable=False)
+    message = Column(String, nullable=False)
+    confirmed_read = Column(Boolean, default=False)
+    reader_id = Column(
+        Integer, ForeignKey("parents.id", ondelete="CASCADE"), nullable=False
+    )
+    writer_id = Column(
+        Integer, ForeignKey("parents.id", ondelete="CASCADE"), nullable=False
+    )
