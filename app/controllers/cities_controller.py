c8606b1b351168800ccbb5aea0f56eecddbcff9e
hirtonsantos <hirtonsantos.hs@gmail.com>
2022-05-03 11:47:16 -0300

    feat(cities): logic to find for capital, state and per page

--- app/controllers/cities_controller.py
+++ app/controllers/cities_controller.py
@@ -1,5 +1,8 @@
+from flask import request
+from ipdb import set_trace
 from http import HTTPStatus
-from flask import jsonify, request
+
+from sqlalchemy import desc, true
 from app.configs.database import db
 from sqlalchemy.orm import Query, Session
 from app.models.cities_model import CityModel
@@ -7,10 +10,48 @@ from app.models.cities_model import CityModel
 
 def retrieve():
     session: Session = db.session
-    response: Query = session.query(CityModel)
+    response: Query = session.query(CityModel).all()
     params = dict(request.args.to_dict().items())
+
     page = int(params.get("page", 1)) - 1
     per_page = int(params.get("per_page", 8))
-    response = response.offset(
-            page * per_page).limit(per_page).all()
-    return {"cities": response}, HTTPStatus.OK
+
+    if params.get("city") and params.get("state"):
+        response: Query = (
+            session.query(CityModel)
+            .filter_by(city=params.get("city"))
+            .filter_by(state=params.get("state"))
+            .all()
+        )
+    elif params.get("city"):
+        response: Query = (
+            session.query(CityModel)
+            .filter_by(city=params.get("city"))
+            .offset(page * per_page)
+            .limit(per_page)
+            .all()
+        )
+    elif params.get("state"):
+        response: Query = (
+            session.query(CityModel)
+            .filter_by(state=params.get("state"))
+            .order_by(desc(CityModel.capital))
+            .offset(page * per_page)
+            .limit(per_page)
+            .all()
+        )
+    cities = []
+    for city in response:
+        city: CityModel
+        city = {
+            "code_ibge": city.code_ibge,
+            "city": city.city,
+            "capital": city.capital,
+            "code_uf": city.code_uf,
+            "uf": city.uf,
+            "state": city.state,
+            "latitude": city.latitude,
+            "longitude": city.longitude,
+        }
+        cities.append(city)
+    return {"cities": cities}, HTTPStatus.OK
