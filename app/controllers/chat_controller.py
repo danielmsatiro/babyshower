52f56ef0678a0e3e6d5f1f150bab928de1cca8a6
Daniel M Satiro <mateus_satiro@yahoo.com.br>
2022-05-04 11:34:00 -0300

    refact[chat]: add instructions

--- app/controllers/chat_controller.py
+++ app/controllers/chat_controller.py
@@ -1,6 +1,13 @@
-def get_chat(other_parent_id):
+def read_chat(other_parent_id):
+    # últimas 10 mensagens -> pode aumentar
+    # alterar para lido -> segue a lógica de um update
+    # alterar model com data de lido
     ...
 
 
 def post_message(other_parent_id):
+    # body
+    # não precisa registrar lido.
+    # data atualizada
+
     ...
