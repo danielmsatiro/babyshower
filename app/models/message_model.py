67563b6a49d2defeb4219d65155fd320983d33eb
hirtonsantos <hirtonsantos.hs@gmail.com>
2022-05-04 20:50:33 -0300

    feat(message): add columns in table to refer parents and mark to read the msgs

--- app/models/message_model.py
+++ app/models/message_model.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 import datetime
 
 from app.configs.database import db
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, Text
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, Text, Boolean
 
 
 @dataclass
@@ -16,6 +16,12 @@ class MessageModel(db.Model):
     id = Column(Integer, primary_key=True)
     data = Column(DateTime, nullable=False, default=now)
     message = Column(Text, nullable=False)
+
+    msg_read = Column(Boolean, nullable=False, default=False)
+
+    parent_id = Column(
+        Integer, ForeignKey("parents.id", ondelete="CASCADE"), nullable=False
+    )
 
     chat_id = Column(
         Integer, ForeignKey("chat.id", ondelete="CASCADE"), nullable=False
